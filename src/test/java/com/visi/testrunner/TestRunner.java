939dea468284e366e3da7ecafdc3e289c8357972
premkumar11-create <prem11064@gmail.com>
2024-07-14 17:20:21 +0530

    Initial Commit

---
+++ src/test/java/com/visi/testrunner/TestRunner.java
@@ -0,0 +1,36 @@
+package com.visi.testrunner;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.visi.service.ServiceTest;
+import com.visi.stepdef.CommonStepDefition;
+import com.visi.utils.WebUtils;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/FeatureFiles", glue = { "com.visi.stepdef",
+		"com.visi.Hooks" }, tags = "@MyntraLogin", dryRun = false, plugin = { "pretty", "json:target/cucumber.json",
+				"html:target/cucumber.html", "rerun:src/test/resource/rerun.txt" })
+public class TestRunner extends WebUtils {
+
+	private static final Logger log = LogManager.getLogger(CommonStepDefition.class);
+
+	@BeforeClass
+	public static void setUp() {
+		ServiceTest.setUp();
+	}
+
+	@AfterClass
+	public static void tearDown() throws IOException {
+		ServiceTest.tearDown();
+	}
+
+}
---
+++ src/test/resources/FeatureFiles/MyntraLogin.feature
@@ -0,0 +1,17 @@
+@MyntraLogin
+Feature: Verify Myntra Login Functionality
+
+Scenario: Verify user able to search any keywords
+Given User navigate to 'mynta' application
+Then User verify 'Home.ProfileIcon' present in the page
+When User clicks on 'Home.ProfileIcon'
+Then User verify 'Login.LoginSignUpButton' present in the page
+When User clicks on 'Login.LoginSignUpButton'
+#Then User verify 'Login.LoginSignUpPageBanner' present in the page
+And User verify 'Login.MobileNumberInputFeild' present in the page
+When User enter '9797910101' in 'Login.MobileNumberInputFeild' field
+And User clicks on 'Login.LoginContinueButton'
+#Then User verify 'Login.VerifyOtpHeader' present in the page
+#When User clicks on 'Login.ContinueWithPassWordLink'
+#Then User verify 'Login.PassWordInputFeild' present in the page
+#When User clicks on 'Login.LoginButton'
